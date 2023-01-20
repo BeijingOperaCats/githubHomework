03214edff67c03310627b2432a5b302fc31ad2c3
初音ミク <4399beijingoperacats@gmail.com>
2023-01-20 18:26:59 +0700

    homework two

---
+++ 2-script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+// 这里是js的注释
